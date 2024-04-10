4163a86a10746f3843260a90321123ba399606db
David Vega Tepetla <pescador2111@hotmail.com>
2024-04-09 22:55:02 -0600

    actividad terminada

---
+++ src/Detalles.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import PeliculaCard from "./PeliculaCard";
+import { useParams } from "react-router-dom";
+import { API_URL } from "./App";
+
+const Detalles = () =>{
+    const {id} = useParams();
+    const [pelicula, setPelicula] = useState();
+
+    const buscarPelicula = async (id) => {
+        let data = null
+        const response = await fetch(`${API_URL}/${id}`);
+        if (response.ok)
+            data = await response.json();
+        setPelicula(data)
+    }
+
+    useEffect(()=>{
+        buscarPelicula(id);
+    }, [id]);
+
+    return (
+        <>
+            {(pelicula != null)
+                ?(
+                    <div className="container">
+                        <PeliculaCard pelicula={pelicula} key={pelicula.peliculaid} />
+                        <div className="movie-desc">{pelicula.sinopsis}</div>
+
+                    </div>
+                ) :
+                (
+                    <div className="empty">
+                        <h2>No hay peliculas encontradas.</h2>
+                    </div>
+                )
+
+            }
+        </>
+    )
+}
+
+export default Detalles;
---
+++ src/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () =>{
+    return (
+        <div className="empty">
+            <h1>Pagina no encontrada</h1>
+        </div>
+    )
+}
+
+export default NotFound;
